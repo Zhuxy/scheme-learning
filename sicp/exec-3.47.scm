de5bceeda09803c747c57c2391228bca20475f68
zhuxiaoyan <zhuxiaoyan@saicmotor.com>
2018-05-02 18:08:34 +0800

    3.50

---
+++ sicp/exec-3.47.scm
@@ -0,0 +1,49 @@
+(library-directories "..")
+(import (modules))
+
+
+;3.47
+;Semaphore
+(define (make-semaphore n)
+    (let ((s n)
+          (mutex (make-mutex)))
+        (define (dispatch m)
+            (cond 
+                ((eq? m `acquire)
+                    (mutex `acquire)
+                    (if (= s 0)
+                        (begin (mutex `release) (dispatch `acquire))
+                        (begin
+                            (set! s (- s 1))
+                            (mutex `release)
+                            #t)))
+                ((eq? m `release)
+                    (mutex `acquire)
+                    (set! s (+ s 1))
+                    (mutex `release)
+                    #t)))
+        dispatch))
+
+
+(define (make-semaphore-1 n)
+    (let ((cell (list #f)))
+        (define (dispatch m)
+            (cond 
+                ((eq? m `acquire)
+                    (if (test-and-set! cell)
+                        (if (= s 0) 
+                            (begin (set-car! cell #f) (dispatch `acquire))
+                            (begin
+                                (set! s (- s 1))
+                                (set-car! cell #f)
+                                #t))
+                        (dispatch `acquire)))
+                ((eq? m `release)
+                    (if (test-and-set! cell)
+                        (begin
+                            (set! s (+ s 1))
+                            (set-car! cell #f)
+                            #t)
+                        (dispatch `release)))))
+        dispatch))
+
