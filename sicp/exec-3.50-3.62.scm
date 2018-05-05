d404e774c0e5bbb8b4cfea176908abe29cc0256a
zhuxiaoyan <zhuxiaoyan@saicmotor.com>
2018-05-05 15:43:54 +0800

    3.59 a

--- sicp/exec-3.50-3.62.scm
+++ sicp/exec-3.50-3.62.scm
@@ -69,7 +69,7 @@
 			(stream-for-each 
 				(lambda (x) 
 					(set! n (+ n 1)) 
-					(if (< n 11) 
+					(if (< n 16) 
 						(begin (display x) (display " "))
 						(begin (display "...") (exit `done)))) s)))
 		(newline)))
@@ -86,6 +86,7 @@
 (display (stream-cdr s))(newline)
 
 (define i (stream-enumerate-interval 1 100))
+(display "(stream-enumerate-interval 1 100): ")
 (display-stream i)
 
 
@@ -107,6 +108,7 @@
 		(+ (stream-car s1) (stream-car s2))
 		(add-streams (stream-cdr s1) (stream-cdr s2))))
 (define s (cons-stream 1 (add-streams s s)))
+(display "(cons-stream 1 (add-streams s s)): ")
 (display-stream s)
 
 ;3.54
@@ -124,6 +126,7 @@
 			(stream-cdr integers) ;从2开始的自然数
 			factorials)))
 
+(display "factorials: ")
 (display-stream factorials)
 
 ;3.55
@@ -134,7 +137,91 @@
 			(stream-cdr s)
 			(partial-sums s))))
 
+(display "(partial-sums integers): ")
 (display-stream (partial-sums integers))
+
+;3.56
+(define (merge s1 s2)
+	(cond 
+		((stream-null? s1) s2)
+		((stream-null? s2) s1)
+		(else
+			(let ((c1 (stream-car s1)) (c2 (stream-car s2)))
+				(cond 
+					((< c1 c2) (cons-stream c1 (merge (stream-cdr s1) s2)))
+					((> c1 c2) (cons-stream c2 (merge s1 (stream-cdr s2))))
+					(else (cons-stream c1 (merge (stream-cdr s1) (stream-cdr s2)))))))))
+
+(define m (merge integers factorials))
+(display "(merge integers factorials): ")
+(display-stream m)
+
+
+(define (scale-stream s factor)
+	(stream-map (lambda (x) (* x factor)) s))
+
+(display "(scale-stream integers 3): ")
+(display-stream (scale-stream integers 3))
+
+(define s (cons-stream 1 (merge (scale-stream integers 2) (merge (scale-stream integers 3) (scale-stream integers 5)))))
+(display "stream with 2 3 5 scales: ")
+(display-stream s)
+
+
+
+;3.57
+;1 1 2 3 5 8 13
+(define fibs 
+	(cons-stream 1
+		(cons-stream 1
+			(add-streams fibs
+				(stream-cdr fibs)))))
+
+(display "fibs with add-streams: ")
+(display-stream fibs)
+
+;3.58
+;(expand 1 7 10)
+;(1 (expand 3 7 10))
+;(1 (4 (expand 2 7 10)))
+;(1 (4 (2 (expand 6 7 10))))
+;(1 (4 (2 (8 (expand 4 7 10)))))
+;(1 (4 (2 (8 (5 (expand 5 7 10))))))
+(define (expand num den radix)
+	(cons-stream
+		(quotient (* num radix) den)
+		(expand (remainder (* num radix) den) den radix)))
+(display "(expand 1 7 10): ")
+(display-stream (expand 1 7 10))
+(display "(expand 3 8 10): ")
+(display-stream (expand 3 8 10))
+
+;3.59 a)
+(define (mul-inv-stream s)
+	(cons-stream
+		(/ 1 (stream-car s))
+		(mul-inv-stream (stream-cdr s))))
+
+(display "mul-inv-stream integers: ")
+(display-stream (mul-inv-stream integers))
+
+(define (integrate-series a)
+	(mul-stream
+		a
+		(mul-inv-stream integers)))
+
+;3.59 b)
+
+;3.60
+;3.61
+;3.62
+
+
+
+
+
+
+
 
 
 
